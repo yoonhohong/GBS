efd857ffe84bf11bc906739f03cd3ddbfb128968
yoonhohong <nrhong@gmail.com>
2016-10-29 16:24:01 +0900

    electrodiagnosis
    nerve conduction study

---
+++ r2p.R
@@ -0,0 +1,8 @@
+# this is a R script for analysis of EDX data of GBS patients 
+
+# start with a long format which contains NCS data of an individual.  
+# may need to convert from wide format which is from EMR. 
+
+
+
+
