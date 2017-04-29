f6f69fe7367b8d1587f08db437cc401e6525e909
yoonhohong <nrhong@gmail.com>
2017-04-29 14:17:42 +0900

    readme

--- r2p.R
+++ r2p.R
@@ -1,7 +1,13 @@
-# this is a R script for analysis of EDX data of GBS patients 
+# this is R script to analyze EDX data of GBS. 
 
-# start with a long format which contains NCS data of an individual.  
-# may need to convert from wide format which is from EMR. 
+# start with a long format which contains raw NCS data of an individual. 
+# convert the raw values into the percentage values relative to upper/lower limits of normal values. 
+# need the reference values. 
+# need the formula to calculate height-adjusted F-wave latency values. 
+
+# i will later develop a script for interpretation based on several published criteria, 
+# and also graphic representations. 
+
 
 
 
